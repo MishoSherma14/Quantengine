6cf1b76143c466008edff290d15a240ae71b4747
MishoSherma14 <164166927+MishoSherma14@users.noreply.github.com>
2025-12-04 18:29:27 +0400

    Update result.go

--- internal/models/result.go
+++ internal/models/result.go
@@ -1,10 +1,13 @@
-package models
-
-type BacktestResult struct {
-	TotalTrades  int
-	WinRate      float64
-	MaxDrawdown  float64
-	ProfitFactor float64
-	Expectancy   float64
-	EquityCurve  []float64
+type ResultOutput struct {
+    StrategyHash string  `bigquery:"strategy_hash"`
+    StrategyJSON string  `bigquery:"strategy_json"`
+    Symbol       string  `bigquery:"symbol"`
+    WinRate      float64 `bigquery:"win_rate"`
+    ProfitFactor float64 `bigquery:"profit_factor"`
+    MaxDD        float64 `bigquery:"max_drawdown"`
+    Sharpe       float64 `bigquery:"sharpe"`
+    AvgReturn    float64 `bigquery:"avg_return"`
+    Score        float64 `bigquery:"score"`
+    EquityCurve  string  `bigquery:"equity_curve"`
+    TS           int64   `bigquery:"ts"`
 }
