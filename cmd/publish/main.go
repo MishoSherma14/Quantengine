ef878bb5005f760270eeec9d24976c5560b34f07
MishoSherma14 <164166927+MishoSherma14@users.noreply.github.com>
2025-12-04 18:03:38 +0400

    Update main.go

--- cmd/publish/main.go
+++ cmd/publish/main.go
@@ -1,14 +1,12 @@
 for _, strat := range strategies {
     for _, symbol := range runner.Markets {
-
         msg := map[string]any{
             "strategy": json.RawMessage(strat),
             "symbol":   symbol,
-            "timeframe": "1D",
+            "timeframe": runner.Timeframe,
         }
 
         data, _ := json.Marshal(msg)
         topic.Publish(ctx, &pubsub.Message{Data: data})
     }
 }
-
